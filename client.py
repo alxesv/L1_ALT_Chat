6704cc7d9eafda4fcb70605fe5da99298825fdb8
alxesv <alexandre.souvorov@gmail.com>
2023-04-05 10:53:52 +0200

    Changed client.py to use class

--- client.py
+++ client.py
@@ -2,44 +2,53 @@ import socket
 import select
 import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP_address = "127.0.0.1"
-Port = 65432
-server.connect((IP_address, Port))
-connected = True
 
-while True:
-	print("Please enter your username")
-	username = (sys.stdin.readline()).strip()
-	if 20 > len(username) > 2:
-		server.send(username.encode())
-		name_taken = server.recv(1024)
-		if name_taken == b'1':
-			print("Name already taken")
-			continue
-		else:
-			break
-	else:
-		print('Select a username between 3 and 20 characters')
+class Client:
+	def __init__(self, ip_address, port):
+		self.IP_address = ip_address
+		self.Port = port
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server.connect((self.IP_address, self.Port))
+		self.connected = True
 
-while connected:
-	sockets_list = [sys.stdin, server]
-	read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
-	for socks in read_sockets:
-		if socks == server:
-			message = (socks.recv(2048)).decode('utf-8')
-			if message == "":
-				connected = False
-				break
-			print(message)
-		else:
-			message = (sys.stdin.readline()).strip()
-			if message == "/exit":
-				connected = False
-				break
-			server.send(message.encode())
-			sys.stdout.write("<You>")
-			sys.stdout.write(message + " \n")
-			sys.stdout.flush()
+	def handle_username(self):
+		while True:
+			print("Please enter your username")
+			username = (sys.stdin.readline()).strip()
+			if 20 > len(username) > 2:
+				self.server.send(username.encode())
+				name_taken = self.server.recv(1024)
+				if name_taken == b'1':
+					print("Name already taken")
+					continue
+				else:
+					break
+			else:
+				print('Select a username between 3 and 20 characters')
 
-server.close()
+	def handle_coms(self):
+		while self.connected:
+			sockets_list = [sys.stdin, self.server]
+			read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+			for socks in read_sockets:
+				if socks == self.server:
+					message = (socks.recv(2048)).decode('utf-8')
+					if message == "":
+						self.connected = False
+						break
+					print(message)
+				else:
+					message = (sys.stdin.readline()).strip()
+					if message == "/exit":
+						self.connected = False
+						break
+					self.server.send(message.encode())
+					sys.stdout.write("<You>")
+					sys.stdout.write(message + " \n")
+					sys.stdout.flush()
+		self.server.close()
+
+
+client = Client("127.0.0.1", 65432)
+client.handle_username()
+client.handle_coms()
